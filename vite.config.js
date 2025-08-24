da750fb5f2f82f5d773affb4b19d5fb37b391f05
tianluoboding <155028747+tianluoboding@users.noreply.github.com>
2025-08-24 21:11:34 +0800

    add deploy workflow

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "wattuapp-prototype", // e.g. "/wattuapp-prototype/"
+  base: "/wattupapp-prototype/",
 });
